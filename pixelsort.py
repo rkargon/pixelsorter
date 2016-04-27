8fe1a806eb71e5cc6369edd388464f6593c7beb5
Raphael Kargon <raphael.kargon@gmail.com>
2016-04-27 15:06:26 -0400

    Integrated tiles, added CLI args for tiles, grouped arguments into a single dict for ease of use

--- pixelsort.py
+++ pixelsort.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 import argparse
-from random import randint
+from random import randint, random
 
 from PIL import Image
 
@@ -13,6 +13,15 @@ def sort_pixels(pixels, size, vertical=False, path=None, max_interval=100, progr
                 key=None, discretize=0, reverse=False):
     """
     Given an image as a list of pixels, applies pixel sorting and returns the new pixels
+    :param discretize: Amount by which to "discretize" pixel values. This is done by dividing each pixel's value
+    (after applying the sort key) by the given amount and then converting to an integer.
+    This would mean that pixels are "binned" into discrete categories, and sorting would be different.
+    Since sorting is stable, this means that pixels with low-level noise remain grouped together,
+    and small details can be preserved in sorting.
+    :param progressive_amount: If this is non-zero,
+    then the sorting interval increases as one progresses row-by-row through the image.
+    progressive_amount indicates the amount, in pixels, by which to increase the sorting interval after each row.
+    :param path: The specific path used to iterate through the image.
     :param pixels: A list of tuples (R,G,B) representing the pixels of the image
     :param size: The size of the image as a tuple (width, height)
     :param vertical: Whether or not the color sorting is applied vertically (the default is horizontal)
@@ -83,6 +92,15 @@ def sort_pixels(pixels, size, vertical=False, path=None, max_interval=100, progr
 
 
 def get_tile_from_image(image, size, top_left_corner, tile_size):
+    """
+    Returns a rectangular region of the given image as a separate image
+    If the tile goes off the edge of the image, it will be truncated. The new size is also returned.
+    :param image: The given image, as a list of (R,G,B) tuples
+    :param size: The size of the image, as (width, height)
+    :param top_left_corner: The top left corner of the tile, relative to the image, as a tuple (x,y)
+    :param tile_size: The size of the tile, as a tuple (width, height)
+    :return: A tuple (tile, size) where the tile is a list of (R,G,B) tuples and the size is (width, height)
+    """
     tile_pixels = []
     # crop tile if necessary
     tile_x = min(size[0] - top_left_corner[0], tile_size[0])
@@ -110,12 +128,36 @@ def apply_tile_to_image(image, size, tile, tile_size, tile_corner):
             image[coords_to_index(img_coords, size[0])] = tile[coords_to_index((x, y), tile_size[0])]
 
 
-def sort_image_tiles(image, size, tile_size, tile_density=1, randomize_tiles=False, **sorting_args):
+def sort_image_tiles(image, size, sorting_args, tile_size, tile_density=1.0, randomize_tiles=False):
+    """
+    Sorts an image by taking various tiles and sorting them individually.
+    :param image: The image to be modified
+    :param size: The size of the image, as (width, height)
+    :param sorting_args: Arguments that would be passed to sort_pixels for each tile
+    :param tile_size: The size of each tile as (width, height)
+    :param tile_density: What fraction of the image is covered in tiles.
+    :param randomize_tiles: Whether tiles should be distributed randomly
+    :return: The modified image
+    """
     out_image = list(image)
     width, height = size
     tile_width, tile_height = tile_size
+    i = 0
     for y in xrange(0, height, tile_height):
         for x in xrange(0, width, tile_width):
+            i += 1
+            if randomize_tiles:
+                # if using randomized tiles, skip a tile with probability 1 - density
+                r = random()
+                if r >= tile_density:
+                    continue
+            else:
+                # if tiles are not randomized, add a tile once every 1/density times
+                if tile_density == 0 or i < 1.0 / tile_density:
+                    continue
+                else:
+                    i -= 1.0 / tile_density
+            # extract a tile, sort it, and copy it back to the image
             tile, current_tile_size = get_tile_from_image(image, size, (x, y), tile_size)
             sorted_tile = sort_pixels(tile, current_tile_size, **sorting_args)
             apply_tile_to_image(out_image, size, sorted_tile, current_tile_size, (x, y))
@@ -144,6 +186,15 @@ def main():
     parser.add_argument("-s", "--sortkey", type=str, default="", help="Function applied to pixels to sort them.")
     parser.add_argument("-v", "--vertical", action='store_true', default=False,
                         help="Whether to pixel-sort vertically instead of horizontally")
+    parser.add_argument("--use-tiles", action='store_true', default=False,
+                        help="Whether to sort the image in tiles")
+    parser.add_argument("--tile-x", type=int, default=100, help="The width of each tile, in pixels")
+    parser.add_argument("--tile-y", type=int, default=100, help="The width of each tile, in pixels")
+    parser.add_argument("--randomize-tiles", action='store_true', default=False,
+                        help="Whether to distribute tiles randomly")
+    parser.add_argument("--tile-density", type=float, default=1.0,
+                        help="Approximately what fraction of the image is covered in tiles")
+
     args = parser.parse_args()
 
     # load image
@@ -153,10 +204,27 @@ def main():
     key = PIXEL_KEY_DICT.get(args.sortkey.lower(), None)
     path = PIXEL_PATH_DICT.get(args.path.lower(), None)
 
-    out_pixels = sort_image_tiles(original_pixels, img.size, (100, 100), randomize=args.randomize, vertical=args.vertical, path=path,
-                             max_interval=args.interval, progressive_amount=args.progressive_amount, discretize=args.discretize, reverse=args.reverse, key=key)
-    # out_pixels = sort_pixels(original_pixels, img.size, randomize=args.randomize, vertical=args.vertical, path=path,
-    #                          max_interval=args.interval, progressive_amount=args.progressive_amount, discretize=args.discretize, reverse=args.reverse, key=key)
+    sorting_args = {
+        'randomize': args.randomize,
+        'vertical': args.vertical,
+        'path': path,
+        'max_interval': args.interval,
+        'progressive_amount': args.progressive_amount,
+        'discretize': args.discretize,
+        'reverse': args.reverse,
+        'key': key,
+    }
+
+    tile_args = {
+        'tile_size': (args.tile_x, args.tile_y),
+        'randomize_tiles': args.randomize_tiles,
+        'tile_density': args.tile_density,
+    }
+
+    if args.use_tiles:
+        out_pixels = sort_image_tiles(original_pixels, img.size, sorting_args=sorting_args, **tile_args)
+    else:
+        out_pixels = sort_pixels(original_pixels, img.size, **sorting_args)
 
     # write output image
     img_out = Image.new(img.mode, img.size)
