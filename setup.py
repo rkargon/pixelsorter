cd132bfd2dfe6624449efebcda883ddb652c1ae7
Raphael Kargon <raphael.kargon@gmail.com>
2016-08-23 23:04:04 -0400

    updated setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
     name='pixelsorter',
     packages=['pixelsorter'],
-    version='1.0',
+    version='1.1',
     description='A powerful and extensible library for pixel-sorting.',
     author='Raphael Kargon',
     author_email='raphael.kargon@gmail.com',
     url='https://github.com/rkargon/pixelsorter',
-    download_url='https://github.com/rkargon/pixelsorter/tarball/1.0',  # I'll explain this in a second
-    keywords=['pixelsorting', 'glitch art', 'pixel', 'sorting'],  # arbitrary keywords
+    download_url='https://github.com/rkargon/pixelsorter/tarball/1.1',
+    keywords=['pixelsorting', 'glitch art', 'pixel', 'sorting'],
     classifiers=[],
 )
